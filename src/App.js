416fabf50db704e7c066f5d59c45d75e2680c855
camille-s <camille.seaberry@gmail.com>
2017-11-07 12:49:35 -0500

    More style tweaks

--- src/App.js
+++ src/App.js
@@ -58,7 +58,7 @@ class App extends Component {
 
     onResize = (w, h) => {
         let width = Math.round(0.9 * w);
-        let height = Math.round(width * 2/3);
+        let height = Math.round(width * 0.55);
 
         this.setState({
             width: width,
